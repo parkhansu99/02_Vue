300df407f1debffb517871fd89fb72d40ccf201e
Parkhansu <0526hans@gmail.com>
2024-05-22 17:51:28 +0900

    05/22 17:51

--- 02-25.js
+++ 02-25.js
@@ -1,5 +1,5 @@
-let obj = { name: '홍길동', age: 20 };
-const proxy = new Proxy(obj, {
+let arr = [10, 20, 30];
+const proxy = new Proxy(arr, {
     get: function (target, key) {
         console.log('## get ' + key);
         if (!target[key]) throw new Error(`존재하지 않는 속성(${key})입니다`);
@@ -11,5 +11,7 @@ const proxy = new Proxy(obj, {
         return (target[key] = value);
     },
 });
+proxy[0] = 98;
 proxy[1] = 99;
-proxy[4] = 100;
+proxy[2] = 100;
+proxy[3] = 101;
