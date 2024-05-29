2c6c0c0f33f4448bc0494fbe53784cad41bb7d82
Parkhansu <0526hans@gmail.com>
2024-05-29 15:40:07 +0900

    5/29 15:40

--- chapter 08/slot-test823/src/components/CheckBox3.vue
+++ chapter 08/slot-test823/src/components/CheckBox3.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
         <slot name="icon"></slot>
-        <input type="checkbox" :value="id" :checked="checked"
-             @change="$emit('check-changed', {id, checked: $event.target.checked })" />
+        <input
+            type="checkbox"
+            :value="id"
+            :checked="checked"
+            @change="
+                $emit('check-changed', { id, checked: $event.target.checked })
+            "
+        />
         <slot name="label">Item</slot>
     </div>
 </template>
 
 <script>
 export default {
-    name : "CheckBox2",
-    props : ["id", "checked"]
-}
+    name: 'CheckBox2',
+    props: ['id', 'checked'],
+};
 </script>
