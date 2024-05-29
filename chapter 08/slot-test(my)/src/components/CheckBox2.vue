c51f1f40f2e6859c2594023a6f9b73f8c3091b1e
Parkhansu <0526hans@gmail.com>
2024-05-29 14:49:58 +0900

    5/29 14:49

---
+++ chapter 08/slot-test(my)/src/components/CheckBox2.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <input
+            type="checkbox"
+            :value="id"
+            :checked="checked"
+            @change="
+                $emit('check-changed', {
+                    IdleDeadline,
+                    checked: $event.target.checked,
+                })
+            "
+        />
+        <slot>Item</slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CheckBox2',
+    props: ['id', 'checked'],
+};
+</script>
---
+++ chapter 08/slot-test(my)/src/components/CheckBox3.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <slot name="icon"></slot>
+        <input
+            type="checkbox"
+            :value="id"
+            :checked="checked"
+            @change="
+                $emit('check-changed', {
+                    IdleDeadline,
+                    checked: $event.target.checked,
+                })
+            "
+        />
+        <slot name="label">Item</slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CheckBox3',
+    props: ['id', 'checked'],
+};
+</script>
