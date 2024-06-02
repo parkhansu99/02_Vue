1ceb0833aede06b73e3eebd2999cc85e2dc50c66
Parkhansu <0526hans@gmail.com>
2024-06-02 18:26:01 +0900

    6/2 18:25

---
+++ chapter 09/todolist-app-compositionAPI-scriptsetup/src/components/InputTodo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row mb-3">
+    <div class="col">
+      <div class="input-group">
+        <input
+          id="msg"
+          type="text"
+          class="form-control"
+          name="msg"
+          placeholder="할일을 여기에 입력!"
+          v-model.trim="todo"
+          @keyup.enter="addTodoHandler"
+        />
+        <span class="btn btn-primary input-group-addon" @click="addTodoHandler"
+          >추가</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+// export default {
+//   name: 'InputTodo',
+//   setup(props, context) {
+
+// const props = defineProps({ todoList: { type: Array, required: true } });
+const emit = defineEmits(['add-todo']);
+
+const todo = ref('');
+const addTodoHandler = () => {
+  if (todo.value.length >= 3) {
+    emit('add-todo', todo.value);
+    todo.value = '';
+  }
+};
+// return { todo, addTodoHandler };
+
+// data() {
+//           return { todo : "" }
+//       },
+//       emits : ["add-todo"],
+//       methods : {
+//           addTodoHandler() {
+//               if (this.todo.length >= 3) {
+//                   this.$emit('add-todo', this.todo);
+//                   this.todo = "";
+//               }
+//           }
+//       },
+</script>
---
+++ chapter 09/todolist-app-compositionAPI-setup( )/src/components/InputTodo.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="row mb-3">
+    <div class="col">
+      <div class="input-group">
+        <input
+          id="msg"
+          type="text"
+          class="form-control"
+          name="msg"
+          placeholder="할일을 여기에 입력!"
+          v-model.trim="todo"
+          @keyup.enter="addTodoHandler"
+        />
+        <span class="btn btn-primary input-group-addon" @click="addTodoHandler"
+          >추가</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'InputTodo',
+  setup(props, context) {
+    const todo = ref('');
+    const addTodoHandler = () => {
+      if (todo.value.length >= 3) {
+        context.emit('add-todo', todo.value);
+        todo.value = '';
+      }
+    };
+    return { todo, addTodoHandler };
+  },
+  // data() {
+  //           return { todo : "" }
+  //       },
+  //       emits : ["add-todo"],
+  //       methods : {
+  //           addTodoHandler() {
+  //               if (this.todo.length >= 3) {
+  //                   this.$emit('add-todo', this.todo);
+  //                   this.todo = "";
+  //               }
+  //           }
+  //       },
+};
+</script>
